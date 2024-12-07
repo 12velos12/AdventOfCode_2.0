b04e5719d942f90dfa21edb1091875b7768def3a
Ryan Scott <ryan144065@gmail.com>
2024-12-07 11:50:23 -0600

    Added Code for Day 3 2024

---
+++ src/main/java/Y2024/D2/P1.java
@@ -0,0 +1,8 @@
+package Y2024.D2;
+
+public class P1 {
+    public static void main(String[] args) {
+        Logic logic = new Logic("src/main/java/Y2024/D2/pInput.txt");
+        logic.runP1Program();
+    }
+}
